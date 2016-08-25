44b4c5c4b4561203f557ba78b8016e2124931377
Olszi37 <olszi73@wp.pl>
2016-08-25 18:19:18 +0200

    implemented carriage class

--- src/main/Java/com/olszi/model/carriage/Carriage.java
+++ src/main/Java/com/olszi/model/carriage/Carriage.java
@@ -1,17 +1,31 @@
 package com.olszi.model.carriage;
 
+import javax.persistence.*;
 import java.util.List;
 
 /**
  * Created by MOlszi on 2016-08-24.
  */
+
+@Entity
+@Table(name = "carriage")
 public class Carriage {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "carriageID")
+    private Long carriageId;
+
+    @Column(name = "trainsetID")
+    @ManyToOne
     private Long trainsetID;
 
+    @Column(name = "number")
     private int number;
 
+    @Column(name = "type")
     private CarriageType type;
 
+    //IDK co wstawic
     private List<CarriageFeatures> features;
 }
