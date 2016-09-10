bfad7fd9223803971652e4722575f86126a7482f
Olszi37 <olszi73@wp.pl>
2016-09-10 22:10:45 +0200

    added StationController

--- src/main/Java/com/olszi/controller/StationController.java
+++ src/main/Java/com/olszi/controller/StationController.java
@@ -1,16 +1,28 @@
 package com.olszi.controller;
 
+import com.olszi.model.Station;
 import com.olszi.service.StationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by MOlszi on 2016-09-10.
  */
 
 @RestController
+@RequestMapping(value = "station")
 public class StationController {
 
     @Autowired
     private StationService stationService;
+
+    @RequestMapping(method = RequestMethod.POST, value = "set/file")
+    public @ResponseBody List<Station> setStationsByFile(){
+        return null;
+    }
 }
