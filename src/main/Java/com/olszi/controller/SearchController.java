a7805c24e413bcc2e36fedc2bd2c03f66e5e76b9
Olszi37 <olszi73@wp.pl>
2016-10-02 12:01:35 +0200

    Added beggining of multi course search

--- src/main/Java/com/olszi/controller/SearchController.java
+++ src/main/Java/com/olszi/controller/SearchController.java
@@ -9,12 +9,14 @@ import com.olszi.service.StationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Siny on 2016-10-01.
@@ -32,7 +34,7 @@ public class SearchController {
     CourseService courseService;
 
     @RequestMapping(method = RequestMethod.POST)
-    public void searchConnections(HttpServletRequest request){
+    public @ResponseBody List<SearchResult> searchConnections(HttpServletRequest request){
         Station initialStop = stationService.getById(new Long(request.getHeader("initialStop")));
         Station finalStop = stationService.getById(new Long(request.getHeader("finalStop")));
 
@@ -42,6 +44,8 @@ public class SearchController {
         List<SearchResult> results = searchWithoutChanges(initialStopResults, finalStopResults);
 
         results.sort((o1, o2) -> o1.getDepartureTime().compareTo(o2.getDepartureTime()));
+
+        return results;
     }
 
     public List<SearchResult> searchWithoutChanges(List<RoutePoint> initialStops, List<RoutePoint> finalStops){
@@ -68,9 +72,19 @@ public class SearchController {
         return results;
     }
 
-    public List<SearchResult> searchWithChanges(List<SearchResult> results, List<RoutePoint> initialStops, List<RoutePoint> finalStops){
+    public List<SearchResult> searchWithChanges(List<SearchResult> results, List<RoutePoint> initialStops,
+                                                List<RoutePoint> finalStops){
 
+        List<List<RoutePoint>> initialCourse = new ArrayList<>();
+        List<List<RoutePoint>> finalCourse = new ArrayList<>();
 
+        //jesli course z initial stop zawiera stacje wspolna z course z final stop
+
+        initialCourse.addAll(initialStops.stream().map(routePoint1 ->
+                routePointService.getByCourse(routePoint1.getCourse())).collect(Collectors.toList()));
+
+        finalCourse.addAll(finalStops.stream().map(routePoint2 ->
+                routePointService.getByCourse(routePoint2.getCourse())).collect(Collectors.toList()));
 
         return results;
     }
